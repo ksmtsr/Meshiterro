e97b51277f93013fd415a72b6ce5927916b73625
kamtsr <116964080+ksmtsr@users.noreply.github.com>
2022-11-13 05:17:43 +0000

    [Update]Meshiterro

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,9 +4,27 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new # form_with に渡すための「空のモデル」
   end
 
+  def create #投稿データの保存
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
+  end
+
   def index
   end
 
   def show
   end
+
+  def destroy
+  end
+
+
+ private #投稿データのストロングパラメータ
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name,:image,:caption)
+  end
+
 end
